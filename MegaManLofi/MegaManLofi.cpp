3df6f6b5d39f2e1d9b8b997385567baf6290562a
Steve <s.elgas@techsmith.com>
2022-12-08 18:42:49 -0500

    better sprite for spikes

--- MegaManLofi/MegaManLofi.cpp
+++ MegaManLofi/MegaManLofi.cpp
@@ -184,7 +184,7 @@ shared_ptr<ConsoleRenderConfig> BuildConsoleRenderConfig()
    // spike that is only impassable upward
    renderConfig->ArenaSpriteMap[2].Width = 1;
    renderConfig->ArenaSpriteMap[2].Height = 1;
-   renderConfig->ArenaSpriteMap[2].Pixels.push_back( { 'M', ConsoleColor::Red } );
+   renderConfig->ArenaSpriteMap[2].Pixels.push_back( { '+', ConsoleColor::Red } );
 
    renderConfig->ArenaSprites = ArenaConsoleSpriteGenerator::GenerateArenaSprites();
 

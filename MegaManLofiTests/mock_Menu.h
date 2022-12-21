18e401f48ddfbff3b5ef219f69d802e7cd2d5cac
Steve <s.elgas@techsmith.com>
2022-12-21 16:04:28 -0500

    Added IMenu interface

---
+++ MegaManLofiTests/mock_Menu.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <gmock/gmock.h>
+
+#include <MegaManLofi/IMenu.h>
+
+class mock_Menu : public MegaManLofi::IMenu
+{
+public:
+   MOCK_METHOD( int, GetOptionCount, ( ), ( const, override ) );
+   MOCK_METHOD( const std::string&, GetOptionTitle, ( ), ( const, override ) );
+   MOCK_METHOD( int, GetSelectedIndex, ( ), ( const, override ) );
+   MOCK_METHOD( void, ExecuteSelectedIndex, ( ), ( override ) );
+};

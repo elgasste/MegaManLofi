b551da548fc472e2207ff763f72022905796a5e0
Steve <s.elgas@techsmith.com>
2023-01-05 10:34:15 -0500

    Added missing parameter to ArenaPortal

--- MegaManLofi/ArenaPortal.h
+++ MegaManLofi/ArenaPortal.h
@@ -9,7 +9,8 @@ namespace MegaManLofi
 
       // If the portal is on the left or right, this is the location of the portal from the
       // top of the arena. If the portal is on the top or bottom, this will be from the left.
-      int TileOffset = 0;
+      int FromTileOffset = 0;
+      int ToTileOffset = 0;
 
       // Basically the size of the portal, in tiles
       int TileRange = 0;

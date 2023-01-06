9392bda7e90f8f76e89bd71ff3216ce64b12514d
Steve <s.elgas@techsmith.com>
2023-01-05 20:50:10 -0500

    Generate entity maps

--- MegaManLofi/EntityDefsGenerator.cpp
+++ MegaManLofi/EntityDefsGenerator.cpp
@@ -6,6 +6,7 @@ using namespace MegaManLofi;
 
 shared_ptr<EntityDefs> EntityDefsGenerator::GenerateEntityDefs()
 {
+   // TODO: remove this stuff when we start using the maps below
    auto entityDefs = make_shared<EntityDefs>();
 
    entityDefs->BulletEntityMetaId = 1;
@@ -13,6 +14,13 @@ shared_ptr<EntityDefs> EntityDefsGenerator::GenerateEntityDefs()
 
    // the player is 152 x 234
    entityDefs->BulletHitBox = { 0, 0, 10, 10 };
+
+   entityDefs->EntityTypeMap[0] = EntityType::Player;
+   entityDefs->EntityTypeMap[1] = EntityType::Projectile; // bullet
+
+   // bullet
+   entityDefs->ProjectileInfoMap[1].HitBox = { 0, 0, 10, 10 }; // player is 152 x 234
+   entityDefs->ProjectileInfoMap[1].Velocity = 2'500;
 
    return entityDefs;
 }

3f8cc6c273d8be88259ee42ecda91d7f06ee3ec1
Steve <s.elgas@techsmith.com>
2023-01-05 22:07:47 -0500

    Remove unused CreateBullet method

--- MegaManLofi/EntityFactory.cpp
+++ MegaManLofi/EntityFactory.cpp
@@ -13,38 +13,6 @@ EntityFactory::EntityFactory( const shared_ptr<EntityDefs> entityDefs,
 {
 }
 
-const shared_ptr<Entity> EntityFactory::CreateBullet( Coordinate<float> position, Direction direction ) const
-{
-   auto bullet = make_shared<Entity>();
-
-   bullet->SetUniqueId( _uniqueNumberGenerator->GetNext() );
-   bullet->SetEntityType( EntityType::Projectile );
-   bullet->SetEntityMetaId( _entityDefs->BulletEntityMetaId );
-   bullet->SetArenaPosition( position );
-   bullet->SetDirection( direction );
-   bullet->SetHitBox( _entityDefs->BulletHitBox );
-
-   if ( direction == Direction::UpLeft || direction == Direction::Left || direction == Direction::DownLeft )
-   {
-      bullet->SetVelocityX( -_entityDefs->BulletVelocity );
-   }
-   else if ( direction == Direction::UpRight || direction == Direction::Right || direction == Direction::DownRight )
-   {
-      bullet->SetVelocityX( _entityDefs->BulletVelocity );
-   }
-
-   if ( direction == Direction::UpLeft || direction == Direction::Up || direction == Direction::UpRight )
-   {
-      bullet->SetVelocityY( -_entityDefs->BulletVelocity );
-   }
-   else if ( direction == Direction::DownLeft || direction == Direction::Down || direction == Direction::DownRight )
-   {
-      bullet->SetVelocityY( _entityDefs->BulletVelocity );
-   }
-
-   return bullet;
-}
-
 const shared_ptr<Entity> EntityFactory::CreateEntity( int entityMetaId, Direction direction ) const
 {
    auto type = _entityDefs->EntityTypeMap[entityMetaId];

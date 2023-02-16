cfb50e458c6d61bbac96fddc380b773df560aa64
selgas <s.elgas@techsmith.com>
2023-02-16 16:05:01 -0500

    Turn off MBC/ASM logging

--- MegaManLofi/MbcAsmDefines.h
+++ MegaManLofi/MbcAsmDefines.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // setting this to 1 will create ASM and MBC log files, which are super useful for debugging
-#define MBCCOMP_ENABLELOGGER              1
+#define MBCCOMP_ENABLELOGGER              0
 
 #define MBCCOMP_MAINFUNC                  "MAIN"
 
